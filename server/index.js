d2c0b173f302100fb38c4532aac8bc38a302a99b
Alex Caron <alex.caron@gmail.com>
2023-04-24 16:26:41 -0500

    add static file routing middleware

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ let app = express();
 // Set up static file service for files in the `client/dist` directory.
 // Webpack is configured to generate files in that directory and
 // this server must serve those files when requested.
+app.use(express.static('../client/dist'));
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
