22066fb3d82de6201e011cf347b78d4f22cd3bbc
Alex Caron <alex.caron@gmail.com>
2023-04-24 18:59:59 -0500

    create a promise-based save function for an array of repos

--- database/index.js
+++ database/index.js
@@ -13,10 +13,34 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+// Input: a repos array from one user
+// Output: returns a promise
+// Side Effects: Save any previously-unsaved repos from this user
+let save = (repos, username) => {
+  // This function should save a repo or repos to the MongoDB
 
+  // Retrieve this user's saved repos from the database
+  // and create an array of their ids to check against
+  // Create a to-save array that is a filtered version of repos
+  // checking whether the saved repos contains each repo
+  // Save each repo's relevant info into a document
+
+  return Repo.find({ user_name: username })
+  .then((savedRepos) => {
+    const savedIds = savedRepos.map((repo) => repo.repo_id);
+    const toSave = savedRepos.filter((repo) => !savedIds.contains(repo.repo_id));
+    return Promise.all(toSave.map((repoToSave) => {
+      const repo = new Repo({
+        repo_id: repoToSave.id,
+        repo_name: repoToSave.name,
+        url: repoToSave.html_url,
+        description: repoToSave.description,
+        user_id: repoToSave.owner.id,
+        user_name: repoToSave.owner.login,
+        popularity: repoToSave.forks_count
+      });
+      return repo.save();
+    }));
+  });
+};
 module.exports.save = save;
